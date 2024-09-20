2e28511f8fbf3696b47dbf24132601b91f8d0753
Sofya Oparina <pstereoo@gmail.com@>
2024-09-20 17:20:53 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на Flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Опарина Софья Александровна, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на Flask</h1>
+
+        <footer>
+            &copy; Опарина Софья, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
