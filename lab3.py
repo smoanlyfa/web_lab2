225fb24b4950b74ef770cf59b8f54f7369bf78de
Sofya Oparina <pstereoo@gmail.com@>
2024-10-16 22:57:19 +0700

    Cookies

--- lab3.py
+++ lab3.py
@@ -1,6 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, make_response, redirect
 lab3 = Blueprint('lab3',__name__)
+
 
 @lab3.route('/lab3/')
 def lab_3():
-    return ''
+    name = request.cookies.get('name')
+    name_color = request.cookies.get('name_color')
+    return render_template('lab3.html', name=name, name_color=name_color)
+
+
+@lab3.route('/lab3/cookie')
+def cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.set_cookie('name', 'Alex', max_age=5)
+    resp.set_cookie('age', '20')
+    resp.set_cookie('name_color', 'green')
+    return resp
