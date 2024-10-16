7237e3f561e587b633cd8d4d9da45eab5c578205
Sofya Oparina <pstereoo@gmail.com@>
2024-10-16 21:09:02 +0700

    Заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, redirect
 app = Flask(__name__)
 from lab1 import lab1
 from lab2 import lab2
-
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
-
+app.register_blueprint(lab3)
 
 
 @app.route("/")
--- lab2.py
+++ lab2.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 lab2 = Blueprint('lab2',__name__)
 
 @lab2.route('/lab2/')
-def lab2():
+def lab_2():
     return render_template('lab2.html')
 
 
