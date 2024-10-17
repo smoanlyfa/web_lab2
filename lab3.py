5fdc3ae8b4ebad51829c42b2c52b395c35ef9658
Sofya Oparina <pstereoo@gmail.com@>
2024-10-17 19:57:04 +0700

    Форма

--- lab3.py
+++ lab3.py
@@ -16,3 +16,11 @@ def cookie():
     resp.set_cookie('age', '20')
     resp.set_cookie('name_color', 'green')
     return resp
+
+
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user = user, age = age, sex = sex)
