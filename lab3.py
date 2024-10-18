57b3cdde8a6d73b28da9ade971a0d93421d408b2
Sofya Oparina <pstereoo@gmail.com@>
2024-10-18 16:55:18 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -29,3 +29,28 @@ def form1():
         errors['age'] = 'Заполните поле!'
     sex = request.args.get('sex')
     return render_template('lab3/form1.html', user = user, age = age, sex = sex, errors=errors)
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black tea':
+        price = 80
+    else:
+        price = 90
+
+    if request.args.get('milk') == 'on':
+        price += 20
+    if request.args.get('sugar') == 'on':
+        price += 10
+    return render_template('lab3/pay.html', price=price)
+
+@lab3.route('/lab3/success')
+def success():
+    return render_template('lab3/success.html')
