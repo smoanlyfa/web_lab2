096e6071637d8f88860ed76dbce87a9823247393
Sofya Oparina <pstereoo@gmail.com@>
2024-11-01 15:47:29 +0700

    Заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ app = Flask(__name__)
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
-
+app.register_blueprint(lab4)
 
 @app.route("/")
 @app.route("/index")
