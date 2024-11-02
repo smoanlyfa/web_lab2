4f0ea8c446062577dc075076df135f6c18d5f29a
Sofya Oparina <pstereoo@gmail.com@>
2024-11-02 09:53:17 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -108,3 +108,28 @@ def logout():
     session.pop('login', None)
     return redirect('/lab4/login')  
 
+@lab4.route('/lab4/fridge', methods=['GET', 'POST'])
+def fridge():
+    message = ''
+    temperature = None
+
+    if request.method == 'POST':
+        try:
+            temperature = float(request.form.get('temperature'))
+            if temperature is None:
+                message = 'Ошибка: не задана температура'
+            elif temperature < -12:
+                message = 'Не удалось установить температуру — слишком низкое значение'
+            elif temperature > -1:
+                message = 'Не удалось установить температуру — слишком высокое значение'
+            elif -12 <= temperature <= -9:
+                message = f'Установлена температура: {temperature}°С ❄️❄️❄️' 
+            elif -8 <= temperature <= -5:
+                message = f'Установлена температура: {temperature}°С ❄️❄️'
+            elif -4 <= temperature <= -1:
+                message = f'Установлена температура: {temperature}°С ❄️' 
+        except ValueError:
+            message = 'Ошибка: не задана температура'
+
+    return render_template('lab4/fridge.html', message=message, temperature=temperature)
+
