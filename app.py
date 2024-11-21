21523a86aa7a7a2bf0f88084ebf79e432eb8ab7e
Sofya Oparina <pstereoo@gmail.com@>
2024-11-21 10:53:03 +0700

    Заготовка к 6-ой лабораторной

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
@@ -14,7 +15,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
-
+app.register_blueprint(lab6)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретный-секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
---
+++ lab6.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, render_template
+lab6 = Blueprint('lab6',__name__)
+
+@lab6.route('/lab6/')
+def lab_6():
+    return render_template('lab6/lab6.html')
