46a051c4167f2eb93fffb20dc7f51588cd4a4c82
Sofya Oparina <pstereoo@gmail.com@>
2024-12-15 18:49:11 +0700

    Получение информации о фильме/-ах по REST

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
@@ -16,6 +17,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретный-секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
