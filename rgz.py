71595bdd1dc0530ffc5b974d6cc1f03e001a8d39
Sofya Oparina <pstereoo@gmail.com@>
2024-12-24 20:20:25 +0700

    Корзина

--- rgz.py
+++ rgz.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, session, redirect, current_app
+from flask import Blueprint, render_template, request, session, redirect, current_app, jsonify, url_for
+from functools import wraps
+from datetime import datetime
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -7,9 +9,236 @@ from os import path
 
 rgz = Blueprint('rgz',__name__)
 
+products = [
+    {
+        'id': 1,
+        'name': 'Кресло "Комфорт"',
+        'description': 'Удобное кресло для отдыха',
+        'price': 15000,
+        'img_url': '/static/rgz/кресло_комфорт.jpg'
+    },
+    {
+        'id': 2,
+        'name': 'Диван "Люкс"',
+        'description': 'Комфортный диван для гостиной',
+        'price': 50000,
+        'img_url': '/static/rgz/диван_люкс.jpg'
+    },
+    {
+        'id': 3,
+        'name': 'Стол "Деревянный"',
+        'description': 'Прочный обеденный стол',
+        'price': 20000,
+        'img_url': '/static/rgz/стол_деревянный.jpg'
+    },
+    {
+        'id': 4,
+        'name': 'Стул "Минимализм"',
+        'description': 'Стильный стул для кухни',
+        'price': 5000,
+        'img_url': '/static/rgz/стул_минимализм.jpg'
+    },
+    {
+        'id': 5,
+        'name': 'Кровать "Альви"',
+        'description': 'Комфортная кровать для спальни',
+        'price': 35000,
+        'img_url': '/static/rgz/кровать_альви.jpg'
+    },
+    {
+        'id': 6,
+        'name': 'Шкаф "Вместительный"',
+        'description': 'Просторный шкаф для одежды',
+        'price': 40000,
+        'img_url': '/static/rgz/шкаф_вместительный.jpg'
+    },
+    {
+        'id': 7,
+        'name': 'Тумба "Компактная"',
+        'description': 'Маленькая тумба для хранения',
+        'price': 8000,
+        'img_url': '/static/rgz/тумба_компактная.jpg'
+    },
+    {
+        'id': 8,
+        'name': 'Стеллаж "Книжный"',
+        'description': 'Стеллаж для книг и декора',
+        'price': 12000,
+        'img_url': '/static/rgz/стеллаж_книжный.jpg'
+    },
+    {
+        'id': 9,
+        'name': 'Кухонный гарнитур',
+        'description': 'Полный комплект для кухни',
+        'price': 120000,
+        'img_url': '/static/rgz/кухонный гарнитур.jpg'
+    },
+    {
+        'id': 10,
+        'name': 'Комод "Классический"',
+        'description': 'Комод с четырьмя ящиками',
+        'price': 18000,
+        'img_url': '/static/rgz/комод_классический.jpg'
+    },
+    {
+        'id': 11,
+        'name': 'Кресло-качалка',
+        'description': 'Удобная качалка для отдыха',
+        'price': 25000,
+        'img_url': '/static/rgz/кресло-качалка.jpg'
+    },
+    {
+        'id': 12,
+        'name': 'Обеденный стол "Круглый"',
+        'description': 'Круглый стол для небольшой семьи',
+        'price': 15000,
+        'img_url': '/static/rgz/обеденный стол_круглый.jpg'
+    },
+    {
+        'id': 13,
+        'name': 'Стул "Металлический"',
+        'description': 'Стул с металлическим каркасом',
+        'price': 6000,
+        'img_url': '/static/rgz/стул_металлический.jpg'
+    },
+    {
+        'id': 14,
+        'name': 'Кровать "Односпальная"',
+        'description': 'Компактная кровать для гостевой комнаты',
+        'price': 20000,
+        'img_url': '/static/rgz/кровать_односпальная.jpg'
+    },
+    {
+        'id': 15,
+        'name': 'Шкаф "Угловой"',
+        'description': 'Угловой шкаф для небольших помещений',
+        'price': 30000,
+        'img_url': '/static/rgz/шкаф_угловой.jpg'
+    },
+    {
+        'id': 16,
+        'name': 'Тумба "Под телевизор"',
+        'description': 'Тумба для размещения телевизора',
+        'price': 10000,
+        'img_url': '/static/rgz/тумба_под телевизор.jpg'
+    },
+    {
+        'id': 17,
+        'name': 'Стеллаж "Многоярусный"',
+        'description': 'Стеллаж с несколькими уровнями',
+        'price': 15000,
+        'img_url': '/static/rgz/стеллаж_многоярусный.jpg'
+    },
+    {
+        'id': 18,
+        'name': 'Кухонный уголок',
+        'description': 'Комплект для небольшой кухни',
+        'price': 80000,
+        'img_url': '/static/rgz/кухонный уголок.jpg'
+    },
+    {
+        'id': 19,
+        'name': 'Комод "Современный"',
+        'description': 'Комод с глянцевым покрытием',
+        'price': 22000,
+        'img_url': '/static/rgz/комод_современный.jpg'
+    },
+    {
+        'id': 20,
+        'name': 'Кресло "Бархатное"',
+        'description': 'Кресло с бархатной обивкой',
+        'price': 18000,
+        'img_url': '/static/rgz/кресло_бархатное.jpg'
+    },
+    {
+        'id': 21,
+        'name': 'Обеденный стол "Прямоугольный"',
+        'description': 'Прямоугольный стол для большой семьи',
+        'price': 25000,
+        'img_url': '/static/rgz/обеденный стол_прямоугольный.jpg'
+    },
+    {
+        'id': 22,
+        'name': 'Стул "Деревянный"',
+        'description': 'Стул из натурального дерева',
+        'price': 7000,
+        'img_url': '/static/rgz/стул_деревянный.jpg'
+    },
+    {
+        'id': 23,
+        'name': 'Кровать "Детская"',
+        'description': 'Кровать для детской комнаты',
+        'price': 25000,
+        'img_url': '/static/rgz/кровать_детская.jpg'
+    },
+    {
+        'id': 24,
+        'name': 'Шкаф "Платяной"',
+        'description': 'Шкаф с полками и вешалками',
+        'price': 45000,
+        'img_url': '/static/rgz/шкаф_платяной.jpg'
+    },
+    {
+        'id': 25,
+        'name': 'Тумба "Подставка"',
+        'description': 'Тумба для декоративных предметов',
+        'price': 9000,
+        'img_url': '/static/rgz/тумба_подставка.jpg'
+    },
+    {
+        'id': 26,
+        'name': 'Стеллаж "Узкий"',
+        'description': 'Узкий стеллаж для узких пространств',
+        'price': 13000,
+        'img_url': '/static/rgz/стеллаж_узкий.jpg'
+    },
+    {
+        'id': 27,
+        'name': 'Кухонный стол',
+        'description': 'Стол для небольшой кухни',
+        'price': 12000,
+        'img_url': '/static/rgz/кухонный стол.jpg'
+    },
+    {
+        'id': 28,
+        'name': 'Комод "Мини"',
+        'description': 'Компактный комод для маленьких комнат',
+        'price': 15000,
+        'img_url': '/static/rgz/комод_мини.jpg'
+    },
+    {
+        'id': 29,
+        'name': 'Стул "Металлик"',
+        'description': 'Стул с металлическим покрытием',
+        'price': 7500,
+        'img_url': '/static/rgz/стул_металлик.jpg'
+    },
+    {
+        'id': 30,
+        'name': 'Кровать "Стандарт"',
+        'description': 'Компактная кровать для спальни',
+        'price': 22000,
+        'img_url': '/static/rgz/кровать_стандарт.jpg'
+    },
+    {
+        'id': 31,
+        'name': 'Шкаф "Мини"',
+        'description': 'Небольшой шкаф для хранения вещей',
+        'price': 18000,
+        'img_url': '/static/rgz/шкаф_мини.jpg'
+    },
+    {
+        'id': 32,
+        'name': 'Тумба "Подсветка"',
+        'description': 'Тумба с подсветкой для гостиной',
+        'price': 12000,
+        'img_url': '/static/rgz/тумба_подсветка.jpg'
+    }
+]
+
 @rgz.route('/rgz/')
 def lab_5():
-    return render_template('rgz/rgz.html', login = session.get('login'), user_name = session.get('user_name'))
+    return render_template('rgz/rgz.html', login = session.get('login'),  user_name=session.get('user_name'), products=[p for p in products])
 
 def db_connect():
     if current_app.config['DB_TYPE'] == 'postgres':
@@ -110,6 +339,86 @@ def login_5():
         return render_template('rgz/login.html', error = 'Логин или пароль неверны')
 
     session['login'] = login
+    session['user_name'] = user['user_name']  
 
     db_close(conn, cur)
     return render_template('rgz/rgzok.html', login=login)
+
+
+@rgz.route('/lab7/rest-api/products/', methods=['GET'])
+def get_products():
+    return jsonify(products)
+
+# Маршрут для получения товара по id
+@rgz.route('/lab7/rest-api/products/<int:id>', methods=['GET'])
+def get_product(id):
+    product = next((p for p in products if p['id'] == id), None)
+    if product:
+        return jsonify(product)
+    else:
+        return jsonify({'error': 'Товар не найден'}), 404
+    
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'login' not in session:
+            return redirect('/rgz/login')
+        return f(*args, **kwargs)
+    return decorated_function
+
+@rgz.route('/rgz/add_to_cart', methods=['POST'])
+def add_to_cart():
+    product_id = request.json.get('product_id')
+    if not product_id:
+        return jsonify({'error': 'Product ID is required'}), 400
+
+    if 'cart' not in session:
+        session['cart'] = {}
+
+    if product_id in session['cart']:
+        session['cart'][product_id] += 1
+    else:
+        session['cart'][product_id] = 1
+
+    session.modified = True
+    print("Current cart:", session['cart'])
+
+    return jsonify({'message': 'Товар добавлен в корзину', 'cart': session['cart']})
+
+@rgz.route('/rgz/cart', methods=['GET'])
+def view_cart():
+    if 'cart' not in session:
+        session['cart'] = {}
+
+    cart_products = [p for p in products if str(p['id']) in session['cart']]
+
+    total_price = sum(p['price'] for p in cart_products)
+    return render_template('rgz/cart.html', cart_products=cart_products, total_price=total_price)
+
+
+@rgz.route('/rgz/remove_from_cart', methods=['POST'])
+def remove_from_cart():
+    product_id = request.json.get('product_id')
+    if not product_id:
+        return jsonify({'error': 'Product ID is required'}), 400
+
+    if 'cart' in session and product_id in session['cart']:
+        del session['cart'][product_id]
+        session.modified = True
+        return jsonify({'message': 'Товар удален', 'cart': session['cart']})
+    else:
+        return jsonify({'error': 'Product not found in cart'}), 404
+    
+
+@rgz.route('/rgz/checkout', methods=['GET', 'POST'])
+def checkout():
+    if request.method == 'GET':
+        return render_template('rgz/checkout.html')
+
+    # Обработка оформления покупки
+    if 'cart' in session:
+        session.pop('cart')
+        return render_template('rgz/checkout_success.html')
+    else:
+        return render_template('rgz/checkout.html', error="Корзина пуста")
---
+++ static/rgzf/main.js
@@ -0,0 +1,91 @@
+function fillProductList() {
+    fetch('/lab7/rest-api/products/')
+    .then(response => response.json())
+    .then(products => {
+        let productGrid = document.querySelector('.product-grid');
+        productGrid.innerHTML = '';
+
+        products.forEach(product => {
+            let productItem = document.createElement('div');
+            productItem.className = 'product-item';
+
+            let productImage = document.createElement('div');
+            productImage.className = 'product-image';
+
+            let img = document.createElement('img');
+            img.src = product.img_url;
+            img.alt = product.name;
+            productImage.appendChild(img);
+
+            let productName = document.createElement('h2');
+            productName.className = 'product-name';
+            productName.innerText = product.name;
+
+            let productPrice = document.createElement('p');
+            productPrice.className = 'product-price';
+            productPrice.innerText = `${product.price} руб.`;
+
+            let productDescription = document.createElement('p');
+            productDescription.className = 'product-description';
+            productDescription.innerText = product.description;
+
+            let addToCartButton = document.createElement('button');
+            addToCartButton.className = 'add-to-cart';
+            addToCartButton.innerText = 'Добавить в корзину';
+            addToCartButton.setAttribute('data-product-id', product.id);
+
+            productItem.appendChild(productImage);
+            productItem.appendChild(productName);
+            productItem.appendChild(productPrice);
+            productItem.appendChild(productDescription);
+            productItem.appendChild(addToCartButton);
+
+            productGrid.appendChild(productItem);
+        });
+    })
+    .catch(error => console.error('Error fetching products:', error));
+}
+
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('add-to-cart')) {
+        const productId = event.target.getAttribute('data-product-id');
+        console.log('Adding product to cart:', productId);
+
+        fetch('/rgz/add_to_cart', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ product_id: productId })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Response from server:', data);
+            alert(data.message);
+        })
+        .catch(error => console.error('Error:', error));
+    }
+});
+
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('remove-from-cart')) {
+        const productId = event.target.getAttribute('data-product-id');
+        console.log('Removing product from cart:', productId);
+
+        fetch('/rgz/remove_from_cart', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ product_id: productId })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Response from server:', data);
+            alert(data.message);
+            // Обновляем корзину на странице
+            location.reload();
+        })
+        .catch(error => console.error('Error:', error));
+    }
+});
