b54b72f2c46a610d790be5cf6447f9a5933c957e
Sofya Oparina <pstereoo@gmail.com@>
2024-12-27 19:23:58 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, session
+from db import db
+from db.models import users8, articles
+from werkzeug.security import generate_password_hash
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -6,3 +9,24 @@ lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8/')
 def lab_8():
     return render_template('lab8/lab8.html')
+
+@lab8.route('/lab8/register/', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    login_exists = users8.query.filter_by(login = login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html', error = 'Такой пользователь уже существует')
+    
+    if not login_form or not password_form:
+        return render_template('lab8/register.html', error='Заполните все поля')
+    
+    password_hash = generate_password_hash(password_form)
+    new_user = users8(login = login_form, password = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8')
