5768e12fcd40aa05cafd239cb0b013c48ae54e18
Sofya Oparina <pstereoo@gmail.com@>
2024-12-27 20:22:05 +0700

    Логаут

--- lab8.py
+++ lab8.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, session
 from db import db
 from db.models import users8, articles
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user 
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -53,7 +53,16 @@ def login():
     return render_template('/lab8/login.html', 
                            error = 'Ошибка входа: логин и/или пароль неверны')
 
+
+
+
 @lab8.route('/lab8/articles/', methods = ['GET', 'POST'])
 @login_required
 def article_list():
-    return "Список статей"
+    return render_template('/lab8/create.html')
+
+@lab8.route('/lab8/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/lab8')
